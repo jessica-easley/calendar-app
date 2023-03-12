2e43ec41795e435d064fa048860c938746b125d4
Easley <jessicaeasley@Jessicas-Air.attlocal.net>
2023-03-11 23:44:32 -0600

    added time blocks to HTML file and updated script.js file

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+// Current Day/Date
+var todayDate = moment().format('dddd, MMM Do YYYY');
+$("#currentDay").html(todayDate);
+
+// Time Section
+function timeTrack() {
+ var currentTime = moment().hour();
+}
+// Time Block For Loops
+
+// Local Storage
+localStorage.setItem(time, text);
+// Retrieve data back from local storage
